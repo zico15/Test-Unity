066641fa7451da2c5b8c35f5294916552d950afc
Eduardo <eduuxx94@gmail.com>
2023-04-22 15:35:40 +0100

    added enemy moves

--- Assets/Scripts/Events/GameEvent.cs
+++ Assets/Scripts/Events/GameEvent.cs
@@ -6,25 +6,45 @@ using UnityEngine.AI;
 public class GameEvent : MonoBehaviour
 {
     public GameObject alvo;
-
     public NavMeshAgent a;
-    // Start is called before the first frame update
-    void Start()
+    public float targetThreshold = 15f; // adjust this value to change the distance threshold
+
+
+    private Animator animator; // Declare the Animator field
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>(); // Initialize the Animator field
     }
 
-    // Update is called once per frame
+    private bool hasTarget = false;
+
     void Update()
+{
+    float distanceToTarget = Vector3.Distance(transform.position, alvo.transform.position);
+
+    if (!hasTarget && distanceToTarget <= targetThreshold)
     {
-      
+        a.SetDestination(alvo.transform.position);
+        hasTarget = true;
+        animator.Play("run_motion");
+        animator.SetBool("Aware", true);
+    }
+    else if (hasTarget && distanceToTarget > targetThreshold)
+    {
+        a.ResetPath();
+        hasTarget = false;
+        animator.Play("idly_motion");
+        animator.SetBool("Aware", false);
+    }
+    else if (hasTarget && distanceToTarget <= targetThreshold)
+    {
         a.SetDestination(alvo.transform.position);
     }
-
+}
 
     public void tes()
     {
         Debug.Log("rer");
     }
-    
 }
